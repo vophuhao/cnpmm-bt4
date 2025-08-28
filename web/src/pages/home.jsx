8222251cae521917d589df4aafb0b3ba560fdcfa
vo phu hao <vophuhao2004@gmail.com>
2025-08-28 10:44:58 +0700

    .

---
+++ web/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import { CrownOutlined } from '@ant-design/icons';
+import { Result } from 'antd';
+const HomePage = () => {
+    return (
+        <div style={{ padding: 20 }}>
+            <Result
+                icon={<CrownOutlined />}
+                title="JSON Web Token (React/Node.JS) iotstar.vn"
+            />
+        </div>
+    )
+}
+export default HomePage;
